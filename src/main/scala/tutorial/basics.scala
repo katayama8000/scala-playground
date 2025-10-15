e35804f70b5a644084b677a7d0d726b307a72fec
katayama8000 <tattu.0310@gmail.com>
2025-10-15 20:14:17 +0900

    add

--- src/main/scala/tutorial/basics.scala
+++ src/main/scala/tutorial/basics.scala
@@ -9,6 +9,31 @@ object Basics {
     println(greeter.greet("World")) // Example usage of Greeter
     val point = Point(1, 3)
     println(s"Point coordinates: (${point.x}, ${point.y})") // Example usage
+
+    // sample of for comprehension with async operations
+    import scala.concurrent.{Future, Await}
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import scala.concurrent.duration._
+    def asyncAdd(x: Int, y: Int): Future[Int] = Future {
+      x + y
+    }
+    val futureResult: Future[Int] = for {
+      a <- asyncAdd(1, 2)
+      b <- asyncAdd(3, 4)
+    } yield a + b
+    val result: Int = Await.result(futureResult, 1.second)
+    println(s"Async result: $result") // Outputs "Async result: 10"
+
+    def asyncAddReturnList(x: Int, y: Int): Future[List[Int]] = Future {
+      List(x + y)
+    }
+    val futureListResult: Future[List[Int]] = for {
+      a <- asyncAddReturnList(1, 2)
+      b <- asyncAddReturnList(3, 4)
+    } yield a ++ b
+
+    val listResult: List[Int] = Await.result(futureListResult, 1.second)
+    println(s"Async list result: $listResult") // Outputs "Async list result:
   }
 
   def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = {
@@ -75,5 +100,52 @@ object Basics {
   def multiplyWithDefault(x: Int)(implicit multiplier: Int): Int =
     x * multiplier
   println(multiplyWithDefault(5)) // Uses implicit multiplier
+
+  // sample of fold method
+  def sumList(numbers: List[Int]): Int = numbers.sum
+  println(sumList(List(1, 2, 3, 4, 5))) // Outputs 15
+
+  // another sample of fold method
+  def concatenateStrings(strings: List[String]): String =
+    strings.fold("")(_ + _)
+  println(
+    concatenateStrings(List("Hello, ", "world", "!"))
+  ) // Outputs "Hello, world!"
+
+  // sample of for yield
+  def squareList(numbers: List[Int]): List[Int] =
+    for (n <- numbers) yield n * n
+  println(squareList(List(1, 2, 3, 4, 5))) // Outputs List(1, 4, 9, 16, 25
+
+  def add(x: Int, y: Int): Int = {
+    x + y
+  }
+
+  private val list1 = List(1, 2, 3)
+  private val list2 = List(4, 5, 6)
+
+  private val combined = for {
+    a <- list1
+    b <- list2
+  } yield (a, b)
+
+  println(
+    combined
+  ) // Outputs List((1,4), (1,5), (1,6), (2,4), (2,5), (2,6), (3,4), (3,5), (3,6))
+
+  val sum: Seq[Int] = for {
+    a <- list1
+    b <- list2
+  } yield add(a, b)
+
+  println(sum) // Outputs List(5, 6, 7, 6, 7, 8, 7, 8, 9)
+
+  // another sample of for yield with filtering
+  def filterAndSquare(numbers: List[Int]): List[Int] = {
+    for { n <- numbers if n % 2 == 0 } yield n * n
+  }
+
+  private val filteredSquared: List[Int] = filterAndSquare(List(1, 2, 3, 4, 5))
+  println(filteredSquared) // Outputs List(4, 16)
 
 }
